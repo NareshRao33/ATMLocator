d2c1d7f00990b5b523dc30297fb01138ec946f55
naresh <nareshrao33@gmail.com>
2015-09-21 01:53:24 -0500

    Initial commit

---
+++ SampleMap/app/src/main/java/com/example/glamantech/samplemap/MapsActivity.java
@@ -0,0 +1,96 @@
+package com.example.glamantech.samplemap;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    double lat,lng;
+    Intent mIntent;
+    ATMLocationDetails mAtmLocationDetails;
+    String locationtype ,distance,name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        mIntent = getIntent();
+        mAtmLocationDetails = mIntent.getParcelableExtra("Location");
+        lat = Double.valueOf(mAtmLocationDetails.getLat());
+        lng = Double.valueOf(mAtmLocationDetails.getLng());
+        name  = mAtmLocationDetails.getName();
+        locationtype = mAtmLocationDetails.getLocationType();
+        distance = mAtmLocationDetails.getDistance() + "mi";
+        setUpMapIfNeeded();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(name + locationtype+"\n"+distance));
+        LatLng latLng = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        Toast.makeText(MapsActivity.this,""+isGooglePlayServicesAvailable(),Toast.LENGTH_LONG).show();
+    }
+    private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
+            return false;
+        }
+    }
+}
---
+++ SampleMap/app/src/main/java/com/example/glamantech/samplemap/ServerResponse.java
@@ -0,0 +1,9 @@
+package com.example.glamantech.samplemap;
+
+/**
+ * Created by Naresh on 9/17/2015.
+ */
+public interface ServerResponse {
+
+    public void setData(String data);
+}
